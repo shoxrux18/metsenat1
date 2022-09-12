3e005d74e9862334570f987faa1dd7e97266efe1
shoxrux002 <turdimoshoxrux@gmail.com>
2022-09-12 22:11:11 +0500

    url

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from .views import StudentRegisterView
-
-urlpatterns = [
-    path('register-student/',StudentRegisterView.as_view(),name='register-student')
-=======
 from django.urls import path, re_path
 from .views import StudentRegisterView, StudentListView, StudentListViews
 
@@ -12,5 +5,4 @@ urlpatterns = [
     path('register-student/', StudentRegisterView.as_view(), name='register-student'),
     path('list-student/', StudentListView.as_view(), name='list_student'),
     path('list-students/<int:pk>/', StudentListViews.as_view(), name='list_students')
->>>>>>> b4f84d110abac91e3e728c058c03343c18dccad5
 ]
